173b02abe0502cb9611185289637bff42dc12764
Shashwath-K <shashwathkukkunoor@outlook.com>
2025-10-30 19:58:11 +0530

    Seperation of concerns

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import PdfCombine from "./PdfCombine";
+import PdfMain from "./PdfMain";
 import PdfCompress from "./PdfCompress";
 import ZipFolder from "./ZipFolder";
 
